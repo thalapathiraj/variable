2d6fa73ba9be90d4892b891b745b10902b8ef910
RAJ <devidevi8506@gmail.com>
2025-10-28 16:30:21 +0530

    Using Variables

---
+++ use_variable.py
@@ -0,0 +1,20 @@
+name="RAJ"
+age=21
+height=5.6
+weight=60.5
+city="MADURAI"
+is_student=False
+marks=443
+pi=3.14
+school_name="Jothi Higher Secondary School"
+my_number=10
+my_text="hello"
+print("my name is",name,"and its type is",type(name))
+print("My age is",age,"and its type is",type(age))
+print("my height is",height,"and my weight is",weight)
+print("Is Student",is_student,"and its type is",type(is_student))
+print("my mark is ",marks)
+print("The value of pi is",pi,"and its type is",type(pi))
+print("My school name is ",school_name)
+print("My number is",my_number,"and its type is",type(my_number))
+print("My text is ",my_text,"and its type is",type(my_text))
